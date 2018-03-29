61143c8008a52c7141f60acacb0f2225f8ac9d9a
oneisme <ansteinlyone@gmail.com>
2018-03-29 10:15:36 +0700

    Create NgameN.sh

---
+++ NgameN.sh
@@ -0,0 +1,436 @@
+#!/system/xbin/bash
+
+2
+
+clear
+
+3
+
+
+
+toilet -f ivrit --#FF66CC "Ngamen Online"
+
+4
+
+
+sleep 1
+
+5
+
+echo "\033[31;1m Mau ngamen dimana guys"
+
+6
+
+sleep 1
+
+7
+
+echo "\033[33;1m Author: Mr.godeyes"
+
+8
+
+sleep 1
+
+9
+
+echo "\033[32;1m ★contack: +6178165732"
+
+10
+
+sleep 1
+
+11
+
+echo "\033[34;1m ★wongNdesoEndonesa"
+
+12
+
+sleep 1
+
+13
+
+echo "\033[36;1m Ngamen dimana guys :"
+
+14
+
+echo "1. Paypal"
+
+15
+
+echo "2. Bank Mandiri"
+
+16
+
+echo "3. BukaLapak"
+
+17
+
+echo "4. Bitcoin"
+
+18
+
+echo "5. Cash Rupiah"
+
+19
+
+echo "6. Rupiah Iklan Otomatis"
+
+20
+
+echo "0. Keluar"
+
+21
+
+echo "\033[30;1m81. Informasi Tool Ini"
+
+22
+
+echo "\033[33;1m Pilih Angka:"
+
+23
+
+read mrrm
+
+24
+
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+
+25
+
+then
+
+26
+
+clear
+
+27
+
+echo "\033[34;1m"
+
+28
+
+figlet "paypal"
+
+29
+
+php 1.php
+
+30
+
+fi
+
+31
+
+32
+
+if
+
+33
+
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+
+34
+
+then
+
+35
+
+clear
+
+36
+
+echo "\033[31;1m"
+
+37
+
+toilet "Bank Mandiri"
+
+38
+
+php 2.php
+
+39
+
+fi
+
+40
+
+41
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+
+42
+
+then
+
+43
+
+clear
+
+44
+
+echo "\033[31;1m"
+
+45
+
+figlet "BukaLapak"
+
+46
+
+php 3.php
+
+47
+
+fi
+
+48
+
+49
+
+50
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+
+51
+
+then
+
+52
+
+clear
+
+53
+
+toilet -f mono12 -F gay "easy cash"
+
+54
+
+echo "\033[30;1m"
+
+55
+
+php 4.php
+
+56
+
+fi
+
+57
+
+58
+
+if
+
+59
+
+[ $mrrm = 5 ] || [ $mrrm = 5 ]
+
+60
+
+then
+
+61
+
+clear
+
+62
+
+toilet -f mono12 -F gay "Bitcoin"
+
+63
+
+echo "\033[33;1m"
+
+64
+
+php 5.php
+
+65
+
+fi
+
+66
+
+67
+
+if
+
+68
+
+[ $mrrm = 6 ] || [ $mrrm = 6 ]
+
+69
+
+then
+
+70
+
+clear
+
+71
+
+toilet -f standard -F gay "rupiah iklan langsung"
+
+72
+
+echo "\033[36;1m"
+
+73
+
+python2 6.py
+
+74
+
+fi
+
+75
+
+76
+
+if
+
+77
+
+[ $mrrm = 81 ] || [ $mrrm = 81 ]
+
+78
+
+then
+
+79
+
+clear
+
+80
+
+toilet -f slant --gay "NgamenOnline"
+
+81
+
+echo "\033[31;1mNama tools: NgamenOnline"
+
+82
+
+sleep 1
+
+83
+
+echo "\033[33;1mKarya: Mr.godeyes (V2 By Mr.IM81)"
+
+84
+
+sleep 1
+
+85
+
+echo "\033[32;1mVersi: v2"
+
+86
+
+sleep 1
+
+87
+
+echo "\033[36;1mTeam: ★wong ndeso endonesa★"
+
+88
+
+sleep 1
+
+89
+
+echo "\033[34;1mInformasi Lebih Lanjut Kunjungi: https://www.github.com/4L13199"
+
+90
+
+echo "\033[31;1m Spesial Thanks To: "
+
+91
+
+echo "\033[36;1m"
+
+92
+
+echo "godeyes"
+
+93
+
+echo "endonesa"
+
+94
+
+echo "wong ndesoTeam"
+
+95
+
+echo "ndesoTeam"
+
+96
+
+echo "godeyes team"
+
+97
+
+sleep 1
+
+98
+
+echo "group dolanan online"
+
+99
+
+echo "\033[30;1m tunggu 8 detik"
+
+100
+
+sleep 8
+
+101
+
+sh NgamenOnline.sh
+
+102
+
+fi
+
+103
+
+104
+
+if
+
+105
+
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+
+106
+
+then
+
+107
+
+echo "\033[31;1m Keluar"
+
+108
+
+sleep 1
+
+109
+
+echo "\033[32;1m Sampai ngamen lagi :)"
+
+110
+
+sleep 1
+
+111
+
+fi
+
+112
+
+
+
+
+
+
