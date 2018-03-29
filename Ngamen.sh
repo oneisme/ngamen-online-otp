2ece4da3555b850257bb420ee3a81229e33ed439
oneisme <ansteinlyone@gmail.com>
2018-03-29 11:20:15 +0700

    Create Ngamen.sh

---
+++ Ngamen.sh
@@ -0,0 +1,221 @@
+
+#!/system/xbin/bash
+
+clear
+
+toilet -f ivrit --gay"Ngamen Online"
+
+echo "\033[31;1m Mau ngamen dimana guys"
+
+echo "\033[33;1m Author: Mr.godeyes"
+
+echo "\033[32;1m ★contack: +6178165732"
+
+echo "\033[34;1m ★wongNdesoEndonesa"
+
+
+echo "\033[36;1m Ngamen dimana guys :"
+
+
+echo "1. Paypal"
+
+
+
+echo "2. Bank Mandiri"
+
+
+
+echo "3. BukaLapak"
+
+
+echo "4. Bitcoin"
+
+
+
+echo "5. Cash Rupiah"
+
+
+echo "\033[30;1m81. Informasi Tool Ini"
+
+
+echo "\033[33;1m Pilih Angka:"
+
+
+read mrrm
+
+
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+
+
+then
+
+
+
+echo "\033[34;1m"
+
+
+
+figlet "paypal"
+
+
+php 1.php
+
+
+
+fi
+
+
+
+
+
+if
+
+
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+
+echo "\033[31;1m"
+
+
+toilet "Bank Mandiri"
+
+
+php 2.php
+
+
+
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+
+
+
+then
+
+
+clear
+
+
+
+echo "\033[31;1m"
+
+
+
+figlet "BukaLapak"
+
+
+php 3.php
+
+
+fi
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+
+
+then
+
+
+clear
+
+
+toilet -f mono12 -F gay "easy cash"
+
+
+
+echo "\033[30;1m"
+
+
+php 4.php
+
+
+fi
+
+
+if
+
+
+[ $mrrm = 5 ] || [ $mrrm = 5 ]
+
+
+then
+
+
+clear
+
+
+
+toilet -f mono12 -F gay "Bitcoin"
+
+
+echo "\033[33;1m"
+
+
+
+php 5.php
+
+
+
+fi
+
+if
+
+68
+
+[ $mrrm = 6 ] || [ $mrrm = 6 ]
+
+
+
+then
+
+
+
+clear
+
+
+toilet -f standard -F gay "rupiah iklan langsung"
+
+
+echo "\033[36;1m"
+
+
+
+python2 6.py
+
+
+
+fi
+
+
+
+if
+
+
+[ $mrrm = 81 ] || [ $mrrm = 81 ]
+
+
+
+then
+
+
+clear
+
+
+toilet -f slant --gay "NgamenOnline"
+
+echo "\033[31;1mNama tools: NgamenOnline"
+
+
+echo "\033[33;1mKarya: Mr.godeyes (V2 By Mr.IM81)"
+
+
+echo "\033[32;1mVersi: v2"
+
+
+echo "\033[36;1mTeam: ★wong ndeso endonesa★"
+
+echo "\033[34;1mInformasi Lebih Lanjut Kunjungi: https://www.github.com/4L13199"
+
+
+
+echo "\033[31;1m Spesial Thanks To: 
+
+echo "godeyes"
